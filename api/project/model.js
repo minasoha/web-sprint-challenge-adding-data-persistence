127abe5762bf345e2e90116bc0949a6b03616b4a
Mina <minasoha@yahoo.com>
2022-01-21 15:34:10 -0500

    wip

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,12 @@
 // build your `Project` model here
+// eslint-disable-next-line no-unused-vars
+const db = require("../../data/dbConfig");
+
+async function findProject() {
+ console.log("foo");
+ return "foo";
+}
+
+module.exports = {
+ findProject,
+};
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,17 @@
 // build your `/api/projects` router here
+const express = require("express");
+const Projects = require("./model");
+
+const router = express.Router();
+
+// eslint-disable-next-line no-unused-vars
+router.get("/", async (req, res, next) => {
+ const project = Projects.findProject();
+ try {
+  res.status(200).json(project);
+ } catch (err) {
+  res.status(404).json({ message: "sorry couldn't find this project" });
+ }
+});
+
+module.exports = router;
